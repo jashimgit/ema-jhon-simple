5dc0babc351cfa1279e94c225fbccdfec20c9d1c
jashimgit <jashim.ncn@gmail.com>
2021-03-05 09:40:05 +0600

    react-router-dom added and router applied

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './components/header/Header';
 import Shop from './components/shop/Shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Review  from './components/review/Review';
+
 function App() {
   return (
     <div>
       <Header />
-      <Shop />
+      <Router>
+        <Switch>
+          <Route path="/shop">
+            <Shop />
+          </Route>
+          <Route path="/review">
+            <Review></Review>
+          </Route>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="*">
+            <NoMatch></NoMatch>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/NoMatch/NoMatch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <div>
+            <h2>Sorry !! Page not found !!</h2>
+            <h4>404 error !!! </h4>
+        </div>
+    );
+};
+
+export default NoMatch;
